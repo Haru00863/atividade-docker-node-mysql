c13fc743b6a989f3b6f0b2dce5caf0c676e3dd65
Haru00863 <haruoltramari@gmail.com>
2026-08-28 23:55:19 +0000

    Testes e mais testes e projeto finalizado

--- init.sql
+++ init.sql
@@ -19,13 +19,13 @@ CREATE TABLE produtos (
 );
 
 INSERT INTO categorias (nome, descricao) VALUES 
-('Eletrônicos', 'Dispositivos eletrônicos e gadgets'),
-('Móveis', 'Móveis para casa e escritório'),
-('Roupas', 'Vestuário masculino e feminino');
+('Eletronicos', 'Dispositivos eletronicos e gadgets'),
+('Moveis', 'Moveis para casa e escritorio'),
+('Roupas', 'Vestuario masculino e feminino');
 
 INSERT INTO produtos (nome, preco, quantidade_estoque, categoria_id) VALUES 
 ('Smartphone', 1500.00, 50, 1),
 ('Notebook', 3500.00, 30, 1),
-('Mesa de Escritório', 450.00, 15, 2),
+('Mesa de Escritorio', 450.00, 15, 2),
 ('Cadeira Gamer', 800.00, 10, 2),
-('Camiseta de Algodão', 50.00, 100, 3);
+('Camiseta de Algodao', 50.00, 100, 3);
